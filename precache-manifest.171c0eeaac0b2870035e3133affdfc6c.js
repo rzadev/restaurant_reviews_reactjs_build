b558b286b5a53fe02135b6acb804edfbea05f543
creative301 <rza9001@gmail.com>
2019-10-09 20:57:48 +0700

    Create restaurant reviews build

---
+++ precache-manifest.171c0eeaac0b2870035e3133affdfc6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2abde7184c2002d95915c0bb9062b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b558853557c9fd6f0fed",
+    "url": "/static/css/main.74462df2.chunk.css"
+  },
+  {
+    "revision": "1d778b5afa6af885e7a1",
+    "url": "/static/js/2.730fe551.chunk.js"
+  },
+  {
+    "revision": "b558853557c9fd6f0fed",
+    "url": "/static/js/main.20e7dc36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
